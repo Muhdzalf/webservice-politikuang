ddbbf96c0bb1e4e4ebb8195efea94051f22f0d39
Muhammad Dzalfiqri Sabani <muhdzalfikri@gmail.com>
2023-01-07 14:04:16 +0700

    rebuild user table structure

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,16 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->char('nik', 16)->primary()->unsigned();
             $table->string('nama', 50);
-            $table->string('email', 40)->unique();
+            $table->string('email', 35)->unique();
+            $table->date('tanggal_lahir');
+            $table->char('jenis_kelamin', 1);
+            $table->string('no_hp', 13);
+            $table->string('alamat', 150);
+            $table->string('pekerjaan', 35);
+            $table->string('kewarganegaraan', 10)->default('Indonesia');
+            $table->string('role', 10)->set('administrator', 'pengawas', 'masyarakat')->default('masyarakat');
             $table->string('password');
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();

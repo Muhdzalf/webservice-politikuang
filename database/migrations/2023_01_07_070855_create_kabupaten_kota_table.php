23fbbee92634fa5ac54c2b6b93763b100e5c79e7
Muhammad Dzalfiqri Sabani <muhdzalfikri@gmail.com>
2023-01-07 14:23:46 +0700

    create provinsi, kabupaten kota, and kecamatan table

---
+++ database/migrations/2023_01_07_070855_create_kabupaten_kota_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kabupaten_kota', function (Blueprint $table) {
+            $table->char('id_kabupaten_kota', 4)->unsigned()->primary();
+            $table->string('nama', 35);
+            $table->unsignedBigInteger('provinsi_id');
+            $table->foreign('provinsi_id')->references('id_provinsi')->on('provinsi')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kabupaten_kota');
+    }
+};
